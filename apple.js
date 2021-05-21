5453a71e76c85ef21f4019240fbb2cf97aee4f3c
yoitsro1234 <72605391+yoitsro1234@users.noreply.github.com>
2021-05-21 12:43:24 +0530

    Update apple.js

--- apple.js
+++ apple.js
@@ -1,5 +1,28 @@
 status = "";
+results = [];
 
 function preload(){
     image("a p p u.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();    
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "Status: Detecting Objects"
+}
+
+function modelLoaded(){
+    console.log("Model Loaded, p o g");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
 }
